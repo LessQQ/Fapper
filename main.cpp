65b400f06ea553172e528b7236b351f3fdf6fd79
Robert Vogel <robert.d.vogel@gmail.com>
2023-08-23 19:33:07 -0500

    fapper - config completed

--- main.cpp
+++ main.cpp
@@ -1,34 +1,74 @@
 #include <iostream>
 
-#include "Iberia.hpp"
-#include "IberiaIO.hpp"
+#include "Iberia/Iberia.hpp"
+#include "Iberia/IberiaIO.hpp"
+
+const std::string path("/etc/fap");
+const std::string fileName("fapper.ids");
 
 bool CheckConfigFile();
+void CreateBase();
+void Help();
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+
     Iberia iberia;
 
-    if (CheckConfigFile())
-    {
-        iberia.LoadDataStore("/etc/fap","fapper.ids");
+    if (CheckConfigFile()) {
+        iberia.LoadDataStore(path.c_str(), fileName.c_str());
     }
-    else
-    {
-        iberia.CreateDataStore("/etc/fap", "fapper.ids");
-        iberia.Add("errors", "~/.fap/error.log");
-        iberia.Add("bank","~/.fap/bank.ids");
-        iberia.Add("release", "~/.fap/release");
-        iberia.Add("tmp","~/.fap/tmp");
-        iberia.SaveDataStore("/etc/fap", "fapper.ids");
+    else {
+        CreateBase();
+    }
+
+    if (argc < 2) {
+        Help();
+        exit(1);
+    }
+
+    std::string command(argv[1]);
+    if (command == "create") {
+        CreateBase();
+    }
+    else if (command == "get") {
+        if (iberia.Contains(argv[2])) {
+            std::cout << iberia.Get(argv[2]) << std::endl;
+        }
+        else {
+            std::cout << "not found!" << std::endl;
+        }
+    }
+    else if (command == "set") {
+        iberia.Add(argv[2],argv[3]);
+        iberia.SaveDataStore(path.c_str(), fileName.c_str());
+    }
+    else if (command == "remove") {
+        iberia.Remove(argv[2]);
+    }
+    else {
+        std::cout << "unknown parameter!" << std::endl;
+        exit(1);
     }
 
     return 0;
 }
 
-bool CheckConfigFile()
-{
+bool CheckConfigFile() {
     IberiaIO iberiaIo;
-    return iberiaIo.FileExist("/etc/fap/fapper.ids");
+    std::string fn = path + "/" + fileName;
+    return iberiaIo.FileExist(fn.c_str());
+}
+void CreateBase() {
+    Iberia iberia;
+
+    iberia.CreateDataStore(path.c_str(), fileName.c_str());
+    iberia.Add("errors", "~/.fap/error.log");
+    iberia.Add("bank","~/.fap/bank.ids");
+    iberia.Add("release", "~/.fap/release");
+    iberia.Add("tmp","~/.fap/tmp");
+    iberia.SaveDataStore(path.c_str(), fileName.c_str());
+}
+void Help() {
+    std::cout << "not enough parameters!" << std::endl;
 }
 
